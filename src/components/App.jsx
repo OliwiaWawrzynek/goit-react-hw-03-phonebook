cafb0c8df40a60c9222875c112fd64d4ff7d7f72
OliwiaWawrzynek <oliwiawawrzynek@gmail.com>
2023-04-01 15:20:09 +0200

    goit-react-hw-03-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -40,6 +40,15 @@ export class App extends Component {
     });
   };
 
+  componentDidMount() {
+    const storageContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (storageContacts) {
+      this.setState({
+        contacts: storageContacts,
+      });
+    }
+  }
+
   componentDidUpdate(prevState) {
     if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
